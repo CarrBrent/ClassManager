fdf4937a6a6f2ed4bb01522383a60f389bf9108f
CarrBrent <958240037@qq.com>
2017-01-19 20:18:38 +0800

    选课功能完成

--- src/app/ui/activity/myclass/CourseSelectActivity.java
+++ src/app/ui/activity/myclass/CourseSelectActivity.java
@@ -20,23 +20,35 @@ import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest;
 
 import myclass.manager.R;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 import app.ui.TitleActivity;
 import app.util.BaseInfo;
 
 public class CourseSelectActivity extends TitleActivity implements OnClickListener{
 	private int cId;
+	private int count=0;
+	private int current = 0;
 	private String cName;
-	private List<List<Map<String,Object>>> selectseminars;
+	private List<Map<String, Object>> currentSeminars= new ArrayList<Map<String,Object>>();
+	private List<List<Map<String,Object>>> selectseminars = new ArrayList<List<Map<String,Object>>>();
 	private String stuListSelectSeminarString = "stuListSelectSeminar.do";
+	private String submitString = "stuSelectSeminar.do";
 	private HttpUtils http = new HttpUtils();
 	private BaseInfo baseInfo;
 	private ListView listView;
+	private Button submit;
+	private RadioGroup radioGroup;
+	private int selectedItemId;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -49,6 +61,22 @@ public class CourseSelectActivity extends TitleActivity implements OnClickListen
 		cId = bundle.getInt("cId");
 		cName = bundle.getString("cName");
 		setTitle(cName);
+
+		submit = (Button)this.findViewById(R.id.submit);
+		submit.setOnClickListener(this);
+
+		radioGroup = (RadioGroup)this.findViewById(R.id.mRadioGroup);
+
+		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { @Override 
+			public void onCheckedChanged(RadioGroup group, int checkedId) { 
+			selectedItemId = checkedId;
+
+		}
+		});
+
+		// 设置缓存1秒,1秒内直接返回上次成功请求的结果
+		http.configCurrentHttpCacheExpiry(1000);
+
 		//通过访问服务器，获取数据
 		RequestParams params = new RequestParams();
 		params.addQueryStringParameter("cid", Integer.toString(cId));
@@ -58,8 +86,63 @@ public class CourseSelectActivity extends TitleActivity implements OnClickListen
 
 	}
 	@Override
+	public void onClick(View v) {
+		super.onClick(v);
+		switch (v.getId()) {
+		case R.id.submit:
+			submit();
+			if (current >= count-1) {
+				finish();
+			}else {
+				current++;
+				SimpleAdapter adapter = new SimpleAdapter(CourseSelectActivity.this,selectseminars.get(current),R.layout.activity_course_select_items,
+						new String[]{"seName"},
+						new int[]{R.id.seName});
+				listView.setAdapter(adapter);
+			}
+			break;
+		default:
+			break;
+		}
+
+	}
+	@Override
 	protected void onBackward(View backwardView) {
 		super.onBackward(backwardView);
+	}
+	private void submit(){
+		SharedPreferences sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE);
+		String sId = sharedPreferences.getString("userId", "null");
+
+		String seId = Integer.toString((Integer)selectseminars.get(current).get(selectedItemId).get("seId"));
+		RequestParams submitParams = new RequestParams();
+		submitParams.addQueryStringParameter("cid", Integer.toString(cId));
+		submitParams.addQueryStringParameter("sid", sId);
+		submitParams.addQueryStringParameter("seid", seId);
+
+
+		http.send(HttpRequest.HttpMethod.GET,
+				baseInfo.getUrl()+submitString,
+				submitParams,
+				new RequestCallBack<String>() {
+
+			@Override
+			public void onStart() {
+			}
+
+			@Override
+			public void onLoading(long total, long current, boolean isUploading) {
+			}
+
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+
+			}
+			@Override
+			public void onFailure(HttpException error, String msg) {
+			}
+		});
+
 	}
 
 	private void GetData(String URL, RequestParams params){
@@ -81,24 +164,24 @@ public class CourseSelectActivity extends TitleActivity implements OnClickListen
 				try {
 					JSONObject jsonObject = new JSONObject(responseInfo.result);
 					JSONArray seminarsJsonArray = new JSONArray(jsonObject.getString("selectseminars"));
+					count =  seminarsJsonArray.length();
+					current = 0;
+					List<Map<String, Object>> seminars= new ArrayList<Map<String,Object>>();
 					for (int i = 0; i < seminarsJsonArray.length(); i++) {
-						
-						List<Map<String, Object>> seminars = getList(seminarsJsonArray.get(i).toString());
+						seminars = getList(seminarsJsonArray.get(i).toString());
 						selectseminars.add(seminars);
 					}
 
 					SimpleAdapter adapter = new SimpleAdapter(CourseSelectActivity.this,selectseminars.get(0),R.layout.activity_course_select_items,
-							new String[]{"cname"},
-							new int[]{R.id.CName});
+							new String[]{"seName"},
+							new int[]{R.id.seName});
 					listView.setAdapter(adapter);
 				} catch (JSONException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				
-				
-				
+
+
 			}
 			@Override
 			public void onFailure(HttpException error, String msg) {
